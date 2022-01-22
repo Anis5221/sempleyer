c6b16ebd64bf3949eb86e2d6887bc4cae1dc66a4
Anis5221 <parsonal494@gmail.com>
2022-01-22 17:37:52 +0600

    header component added

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import './Header.css';
+const Header = () => {
+    return ( <
+        >
+        <
+        div className = "main-header" >
+        <
+        div >
+        <
+        h1 > Starter Software Firm < /h1> < /
+        div > <
+        nav >
+        <
+        ul >
+        <
+        li > Home < /li> <
+        li > About < /li> <
+        li > Contact < /li> < /
+        ul >
+
+        <
+        /nav> < /
+        div > <
+        />
+    );
+}
+
+export default Header;
